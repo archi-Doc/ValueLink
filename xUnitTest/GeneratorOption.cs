9db6868747c75c39b0bb22f8c98f00e386669bab
archi-Doc <archidoc422@gmail.com>
2022-11-13 20:03:32 +0900

    .

--- xUnitTest/GeneratorOption.cs
+++ xUnitTest/GeneratorOption.cs
@@ -1,12 +1,13 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System;
+using Tinyhand;
 using ValueLink;
 
 namespace xUnitTest
 {
     [ValueLinkGeneratorOption(AttachDebugger = false, GenerateToFile = false)]
-    [Tinyhand.TinyhandGeneratorOption(AttachDebugger = false, GenerateToFile = false)]
+    [TinyhandGeneratorOption(AttachDebugger = false, GenerateToFile = false)]
     internal class GeneratorOption
     {
     }
