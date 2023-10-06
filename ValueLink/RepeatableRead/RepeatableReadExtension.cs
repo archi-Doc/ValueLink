19976e3df703c026a243a26481e08229a93d7181
archi-Doc <archidoc422@gmail.com>
2023-10-06 21:04:37 +0900

    .

---
+++ ValueLink/RepeatableRead/RepeatableReadExtension.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace ValueLink;
+
+public static class RepeatableReadExtension
+{
+    public static bool IsValid(this RepeatableObjectState state)
+        => state == RepeatableObjectState.Valid;
+
+    public static bool IsInvalid(this RepeatableObjectState state)
+        => state != RepeatableObjectState.Valid;
+
+    public static bool IsValid(this RepeatableGoshujinState state)
+        => state == RepeatableGoshujinState.Valid;
+
+    public static bool IsInvalid(this RepeatableGoshujinState state)
+        => state != RepeatableGoshujinState.Valid;
+}
