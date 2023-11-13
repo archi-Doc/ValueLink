f0cf63e85bfa95be8cb4c5925853296d7e185174
archi-Doc <archidoc422@gmail.com>
2023-11-13 23:47:36 +0900

    .

---
+++ xUnitTest/CrystalTest.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Tinyhand;
+using ValueLink;
+using Xunit;
+
+namespace xUnitTest.CrystalDataTest;
+
+[TinyhandObject(Structual = true)]
+[ValueLinkObject(Isolation = IsolationLevel.RepeatableRead)]
+public partial record CreditData
+{
+    public CreditData()
+    {
+    }
+
+    [Link(Primary = true, Unique = true, Type = ChainType.Unordered)]
+    [Key(0, AddProperty = "Credit", PropertyAccessibility = PropertyAccessibility.GetterOnly)]
+    private int credit;
+
+    // [Key(3, AddProperty = "Borrowers", PropertyAccessibility = PropertyAccessibility.GetterOnly)]
+    // private StorageData<Borrower.GoshujinClass> borrowers = new();
+}
+
+[TinyhandObject(Structual = true)]
+[ValueLinkObject(Isolation = IsolationLevel.RepeatableRead)]
+public sealed partial record Borrower // : ITinyhandCustomJournal
+{
+    public Borrower()
+    {
+    }
+
+    [Key(0)]
+    [Link(Unique = true, Primary = true, Type = ChainType.Unordered)]
+    private long id;
+}
