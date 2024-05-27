541b04ec61e647bdf9d18fcfbfc7077b1d18157a
archi-Doc <archidoc422@gmail.com>
2024-05-27 23:18:55 +0900

    .

--- Playground/IntegralityTest/IntegralityEngine.cs
+++ Playground/IntegralityTest/IntegralityEngine.cs
@@ -1,34 +1,100 @@
-﻿using System.Collections;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 using Arc.Collections;
+using Arc.Unit;
+using Playground;
+using Tinyhand.IO;
+using ValueLink;
+
+#pragma warning disable CS1998
 
 namespace Tinyhand.Integrality;
 
-public class IntegralityEngine<T>
-    where T : IStructualObject
-{// Integrate/Differentiate
-    private static IntegralityContext<T> defaultContext;
+public class TestIntegralityEngine : IntegralityEngine<Message.GoshujinClass, Message>
+{
+    public static readonly ObjectPool<TestIntegralityEngine> Pool = new(() => new());
 
-    static IntegralityEngine()
+    public override bool Validate(Message obj)
+        => true;
+
+    public override void Prune(Message.GoshujinClass goshujin)
     {
-        defaultContext = new();
+    }
+}
+
+public class IntegralityEngine<TGoshujin, TObject>
+    where TGoshujin : IGoshujin, IIntegrality
+    where TObject : ITinyhandSerialize<TObject>, IIntegrality
+{// Integrate/Differentiate
+    public delegate Task<(IntegralityResult Result, BytePool.RentMemory Difference)> DifferentiateDelegate(BytePool.RentMemory integration, CancellationToken cancellationToken);
+
+    static public async Task<(IntegralityResult Result, BytePool.RentMemory Difference)> Differentiate(TGoshujin obj, BytePool.RentMemory integration)
+    {
+        return (IntegralityResult.Success, default);
     }
 
     public IntegralityEngine()
     {
-        this.Context = defaultContext;
     }
 
-    public IntegralityContext<T> Context { get; set; }
+    #region FieldAndProperty
 
-    public int Level { get; private set; }
+    private IntegralityState state = IntegralityState.Probe;
+    private ulong targetHash;
 
-    public void Integrate(T obj, BytePool.RentMemory difference, out BytePool.RentMemory integration)
+    #endregion
+
+    public async Task<IntegralityResult> Integrate(TGoshujin obj, DifferentiateDelegate differentiateDelegate, CancellationToken cancellationToken = default)
     {
-        integration = default;
+        if (this.state == IntegralityState.Probe)
+        {// Probe
+            var primaryHash = obj.GetIntegralityHash();
+            var rentArray = BytePool.Default.Rent(sizeof(ulong));
+            var rentMemory = rentArray.AsMemory(0, sizeof(ulong));
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(rentMemory.Span), primaryHash);
+            var r = await differentiateDelegate(rentMemory, cancellationToken).ConfigureAwait(false);
+            if (r.Result != IntegralityResult.Success)
+            {
+                return r.Result;
+            }
+
+            var memory = r.Difference.Memory;
+            if (memory.Length < sizeof(ulong))
+            {
+                return IntegralityResult.InvalidData;
+            }
+
+            this.targetHash = BitConverter.ToUInt64(memory.Span);
+            if (primaryHash == this.targetHash)
+            {
+                return IntegralityResult.Success;
+            }
+
+            this.state = IntegralityState.Request;
+        }
+
+        if (this.state == IntegralityState.Request)
+        {
+        }
+
+        void Internal()
+        {
+            //var writer = new TinyhandWriter(owner.ByteArray);
+            //writer.WriteUInt8((byte)state);
+
+        }
+
+
+        return IntegralityResult.Success;
     }
 
-    public void Differentiate(T obj, BytePool.RentMemory integration, out BytePool.RentMemory difference)
+    public virtual bool Validate(TObject obj)
+        => true;
+
+    public virtual void Prune(TGoshujin goshujin)
     {
-        difference = default;
     }
 }
