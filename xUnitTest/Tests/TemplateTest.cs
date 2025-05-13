bb0d0b0a087f37d875d6b3d5bb6517321436f484
archi-Doc <archidoc422@gmail.com>
2025-05-13 18:40:45 +0900

    .

---
+++ xUnitTest/Tests/TemplateTest.cs
@@ -0,0 +1,13 @@
+// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Xunit;
+
+namespace xUnitTest;
+
+public class TemplateTest
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
