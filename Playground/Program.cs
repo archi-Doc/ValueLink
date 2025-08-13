a61a08b3c6c4976d2f0444e94b4903884be0ef00
archi-Doc <archidoc422@gmail.com>
2025-08-13 17:20:43 +0900

    .

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,50 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using Tinyhand;
 using ValueLink;
 using ValueLink.Integrality;
 
 namespace Playground;
-
-[TinyhandObject]
-[ValueLinkObject(Integrality = true, Isolation = IsolationLevel.Serializable)]
-public partial class SimpleIntegralityClass : IEquatableObject<SimpleIntegralityClass>
-{
-    public class Integrality : Integrality<GoshujinClass, SimpleIntegralityClass>
-    {
-        public static readonly Integrality Default = new()
-        {
-            MaxItems = 100,
-            RemoveIfItemNotFound = true
-        };
-    }
-
-    public SimpleIntegralityClass()
-    {
-    }
-
-    public SimpleIntegralityClass(int id, string name)
-    {
-        this.Id = id;
-        this.Name = name;
-    }
-
-    [Key(0)]
-    [Link(Primary = true, Unique = true, Type = ChainType.Unordered)]
-    public partial int Id { get; private set; }
-
-    [Key(1)]
-    public string Name { get; set; } = string.Empty;
-
-    [IgnoreMember]
-    [Link(Type = ChainType.Ordered)]
-#pragma warning disable CLG016 // No Key attribute
-    public partial int Age { get; protected set; }
-#pragma warning restore CLG016 // No Key attribute
-
-    bool IEquatableObject<SimpleIntegralityClass>.ObjectEquals(SimpleIntegralityClass other)
-        => this.Id == other.Id && this.Name == other.Name;
-}
 
 internal class Program
 {
